a068917a13d83a1911422bc797ef7cdece98c717
florine.pawlowski.etu <florine.pawlowski.etu@univ-lille.fr>
2024-04-02 14:23:23 +0200

    update n°10 : created an exception to replace Throwable caught objects in common.base

---
+++ guava/src/com/google/common/base/exceptions/ImpossibleToCleanException.java
@@ -0,0 +1,10 @@
+package com.google.common.base.exceptions;
+
+/**
+ * public class exception for cleaning time
+ */
+public class ImpossibleToCleanException extends Throwable{
+    public ImpossibleToCleanException(String message){
+        super(message);
+    }
+}
